fecddec752f0c1ca49a7cf9291b1385fbcdb7f9a
Vincent Mwangi <vincentmwangi478@gmail.com>
2026-08-11 09:45:25 +0300

    assets

--- assets/js/wp-config.js
+++ assets/js/wp-config.js
@@ -1,6 +1,6 @@
 window.VINCE_WP_PORTFOLIO = {
   source: 'wpcom-posts',
-  apiBase: '',
+  apiBase: 'https://vinceportfoliocms.wordpress.com',
   mockDataUrl: '',
   endpoint: '/wp-json/wp/v2/posts',
   perPage: 50
